14ddc76c8b840ef62a4f3c6c15de37dde1c99dd5
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2022-07-22 02:07:52 +0530

    changes

--- Containers/Unordered_Associative_Containers/unordered_set.cpp
+++ Containers/Unordered_Associative_Containers/unordered_set.cpp
@@ -2,12 +2,73 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
+typedef unsigned long long ull;
+
+// Debug Module
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " : "; _print(x); cerr << endl;
+#endif
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template<class T>
+void _print(unordered_set<T> v) {
+	cerr << "{ ";
+	for (T i : v) {
+		_print(i);
+		cerr << " ";
+	}
+	cerr << "}";
+}
+
+template<class T>
+void _print(unordered_set<T, greater<T>> v) {
+	cerr << "{ ";
+	for (T i : v) {
+		_print(i);
+		cerr << " ";
+	}
+	cerr << "}";
+}
 
 class Solution {
 public:
 	void solve() {
-		int n; cin >> n;
-		cout << n << "\n";
+		int arr[] = {2, 5, 2, 1, 5, 5, 2, 2, 1, 6, 6, 7};
+		unordered_set<int> st1;
+
+		// Insert elements
+		cerr << "arr : [ ";
+		for (auto e : arr) {
+			cerr << e << " ";
+			st1.insert(e);	// O(log(n)) time, n = size of unordered_set
+		}
+		cerr << "]\n";
+		debug(st1);
+		int len = st1.size();
+		debug(len);
+
+		// Erase elements
+		st1.erase(st1.begin()); // st.erase(iterator)
+		debug(st1);
+		st1.erase(st1.find(5)); // st.erase(iterator)
+		debug(st1);
+		st1.erase(6); // st.erase(value)
+		debug(st1);
+		st1.erase(st1.begin(), st1.find(7)); // st.erase(startItr, endItr)
+		debug(st1);
+
+		// Copy
+		unordered_set<int> st2(st1.begin(), st1.end());
+		debug(st2);
+		auto it = st2.find(6);
+		debug(*it);
+		cerr << "holululu\n";
 	}
 };
 
